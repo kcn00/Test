8ff5b96bf91c618df1eafac1929f06ab3d302ba0
kcn00 <qasw2357@naver.com>
2022-04-11 23:43:17 +0900

    ver.2

---
+++ Vincent_Code/The_Bedroom_at_Arles.cpp
@@ -0,0 +1,6 @@
+#include "header.h"
+
+void The_Bedroom_at_Arles() {
+	char title[30]="The Bedroom at Arles";
+	cout<<"Title: "<<title<<endl;
+}
--- Vincent_Code/header.h
+++ Vincent_Code/header.h
@@ -4,3 +4,4 @@ using namespace std;
 void Almond_Blossom();
 void Sunflowers();
 void The_Starry_Night();
+void The_Bedroom_at_Arles();
